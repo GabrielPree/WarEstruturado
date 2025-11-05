43167bcadbd8f8c0a1f8f47783ee2ad720ff08cc
Gabriel Pree <gabriellopesp@hotmail.com>
2025-11-05 19:31:55 -0300

    Feita a modularização do codigo e adicionado 3 missões escolhidas aleatoriamente para o jogador.

--- warestruturado.c
+++ warestruturado.c
@@ -15,10 +15,89 @@ struct Territorio {
     int tropas;
 };
 
-// --- Limpa o buffer do teclado ---
-void limpaBufferEntrada() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+// --- Estrutura do Jogador ---
+struct Jogador {
+    char cor[TAM_STRING];
+    int missao;
+    char corAlvo[TAM_STRING];
+};
+
+// --- Protótipos das funções ---
+void cadastrarTerritorios(struct Territorio *territorios, int *numTerritorios);
+void listarTerritorios(struct Territorio *territorios, int total);
+void atacar(struct Territorio *atacante, struct Territorio *defensor);
+void escolhaCor(struct Territorio *territorios, struct Jogador *jogador, int numTerritorios);
+void sortearMissao(struct Jogador *jogador, struct Territorio *territorios, int numTerritorios);
+void verificarMissao(struct Territorio *territorios, int numTerritorios, struct Jogador *jogador);
+void menu(struct Territorio *territorios, int numTerritorios, struct Jogador *jogador);
+void limpaBufferEntrada();
+
+// --- Função principal ---
+int main() {
+    srand(time(NULL));
+
+    int numTerritorios = 0;
+
+    // --- Alocação dinâmica ---
+    struct Territorio *territorios = (struct Territorio *)calloc(MAX_TERRITORIOS, sizeof(struct Territorio));
+    if (territorios == NULL) {
+        printf("Erro ao alocar memória!\n");
+        return 1;
+    }
+
+    struct Jogador *jogador = (struct Jogador *)calloc(1, sizeof(struct Jogador));
+    if (jogador == NULL) {
+        printf("Erro ao alocar memória!\n");
+        return 1;
+    }
+
+
+    // --- Inicia o cadastro dos territórios ---
+    cadastrarTerritorios(territorios, &numTerritorios);
+    
+    // --- Exibe o estado inicial do mapa ---
+    listarTerritorios(territorios, numTerritorios);
+
+    // --- Sorteia a cor do jogador ---
+    escolhaCor(territorios, jogador, numTerritorios);
+
+    // --- Sorteia a missão ---
+    sortearMissao(jogador, territorios, numTerritorios);
+
+    // exibe menu de ataque
+    menu(territorios, numTerritorios, jogador);
+
+
+    printf("\n--- Fim da Guerra ---\n");
+    free(territorios);
+    free(jogador);
+    return 0;
+}
+
+//================= IMPLEMENTAÇÃO DAS FUNÇÕES ========================
+
+ // --- Função para cadastrar territórios ---
+void cadastrarTerritorios(struct Territorio *territorios, int *numTerritorios) {
+    printf("\n=======================");
+    printf("\n=   WAR ESTRUTURADO   =\n");
+    printf("=======================\n");
+    while (*numTerritorios < MAX_TERRITORIOS) {
+        int indice = *numTerritorios;
+        printf("\n--- Cadastrando o território %d ---\n", indice + 1);
+        printf("Digite o nome do território: ");
+        fgets(territorios[indice].nome, TAM_STRING, stdin);
+        territorios[indice].nome[strcspn(territorios[indice].nome, "\n")] = '\0';
+
+        printf("Digite a cor do exército: ");
+        fgets(territorios[indice].cor, TAM_STRING, stdin);
+        territorios[indice].cor[strcspn(territorios[indice].cor, "\n")] = '\0';
+
+        printf("Digite o número de tropas: ");
+        scanf("%d", &territorios[indice].tropas);
+        limpaBufferEntrada();
+
+        (*numTerritorios)++;
+    }
 }
 
 // --- Função para listar territórios ---
@@ -64,42 +143,9 @@ void atacar(struct Territorio *atacante, struct Territorio *defensor) {
     }
 }
 
-// --- Função principal ---
-int main() {
-    srand(time(NULL));
-
-    int numTerritorios = 0;
-
-    // --- Alocação dinâmica ---
-    struct Territorio *territorios = (struct Territorio *)calloc(MAX_TERRITORIOS, sizeof(struct Territorio));
-    if (territorios == NULL) {
-        printf("Erro ao alocar memória!\n");
-        return 1;
-    }
-
-    // --- Cadastro dos territórios ---
-    while (numTerritorios < MAX_TERRITORIOS) {
-        printf("\n--- Cadastrando o território %d ---\n", numTerritorios + 1);
-        printf("Digite o nome do território: ");
-        fgets(territorios[numTerritorios].nome, TAM_STRING, stdin);
-        territorios[numTerritorios].nome[strcspn(territorios[numTerritorios].nome, "\n")] = '\0';
-
-        printf("Digite a cor do exército: ");
-        fgets(territorios[numTerritorios].cor, TAM_STRING, stdin);
-        territorios[numTerritorios].cor[strcspn(territorios[numTerritorios].cor, "\n")] = '\0';
-
-        printf("Digite o número de tropas: ");
-        scanf("%d", &territorios[numTerritorios].tropas);
-        limpaBufferEntrada();
-
-        numTerritorios++;
-    }
-
-    // --- Exibe o estado inicial do mapa ---
-    listarTerritorios(territorios, numTerritorios);
-
+void loopAtaque(struct Territorio *territorios, int numTerritorios) {
     int iAtacante, iDefensor;
-
+    printf("\n--- Fase de ataques ---\n");
     // --- Loop de ataques ---
     do {
         printf("\nEscolha o número do território atacante (1 a %d) ou 0 para sair: ", numTerritorios);
@@ -134,13 +180,130 @@ int main() {
         atacar(&territorios[iAtacante - 1], &territorios[iDefensor - 1]);
         
         // --- Pausa para leitura do ataque---
-        printf("\nPrecione Enter para continuar para o proximo turno.\n");
+        printf("\nPressione Enter para continuar para o proximo turno.\n");
         getchar();
 
         // --- Exibe o estado atualizado do mapa ---
         listarTerritorios(territorios, numTerritorios);
     } while (iAtacante != 0);
-    printf("\n--- Fim da Guerra ---\n");
-    free(territorios);
-    return 0;
+}
+
+// --- Função para escolher a cor do exército ---
+void escolhaCor(struct Territorio *territorios,struct Jogador *jogador, int numTerritorios) {
+
+    int indice = rand() % numTerritorios; // sorteia um índice de território existente
+    strcpy(jogador->cor, territorios[indice].cor);
+
+    printf("\n-------------------------------------\n");
+    printf("  Sua cor de exército foi sorteada!  \n");
+    printf("  Você será o exército %s.\n", jogador->cor);
+    printf("-------------------------------------\n");
+}
+
+// --- Função para sortear a missão ---
+void sortearMissao(struct Jogador *jogador, struct Territorio *territorios, int numTerritorios) {
+    jogador->missao = rand() % 3;
+    int corAleatoria = rand() % numTerritorios;
+
+    printf("\n--- SUA MISSÃO ---\n");
+    switch (jogador->missao) {
+        case 0:
+            printf("Conquistar 2 territórios.\n");
+            break;
+        case 1:
+            do {
+            corAleatoria = rand() % numTerritorios;
+            strcpy(jogador->corAlvo, territorios[corAleatoria].cor);
+            } while (strcmp(jogador->corAlvo, jogador->cor) == 0);
+            printf("Destruir o exército %s.\n", jogador->corAlvo);
+            break;
+        case 2:
+            printf("Manter 2 territórios com 3 ou mais tropas.\n");
+            break;
+    }
+}
+
+// --- Função para verificar se a missão foi cumprida ---
+void verificarMissao(struct Territorio *territorios, int numTerritorios, struct Jogador *jogador) {
+    int cont = 0;
+    int cumpriu = 0;
+
+    switch (jogador->missao) {
+        case 0: // Conquistar 2 territórios
+            for (int i = 0; i < numTerritorios; i++) {
+                if (strcmp(territorios[i].cor, jogador->cor) == 0)
+                    cont++;
+            }
+            if (cont >= 2) cumpriu = 1;
+            break;
+
+        case 1: // Destruir o exército alvo sorteado
+            for (int i = 0; i < numTerritorios; i++) {
+            if (strcmp(territorios[i].cor, jogador->corAlvo) == 0)
+                cont++;
+            }
+            if (cont == 0) cumpriu = 1;
+            break;
+
+        case 2: // Manter 2 territórios com 3 ou mais tropas
+            for (int i = 0; i < numTerritorios; i++) {
+                if (strcmp(territorios[i].cor, jogador->cor) == 0 && territorios[i].tropas >= 3)
+                    cont++;
+            }
+            if (cont >= 2) cumpriu = 1;
+            break;
+    }
+
+    // Mostra resultado dentro da função
+    if (cumpriu)
+        printf("\nMissão concluída! Parabéns, você venceu!\n");
+    else
+        printf("\nMissão ainda não concluída.\n");
+}
+
+// --- Função para exibir o menu de ataque ---
+void menu(struct Territorio *territorios, int numTerritorios, struct Jogador *jogador) {
+    int missaoSorteada = jogador->missao;
+    int opcao;
+
+    do {
+        printf("\n--- MENU DE AÇÕES ---\n");
+        printf("1 - Atacar\n");
+        printf("2 - Verificar Missao\n");
+        printf("0 - Sair\n");
+        printf("Escolha sua ação: ");
+        scanf("%d", &opcao);
+        limpaBufferEntrada();
+
+        switch (opcao) {
+            case 1: {
+                loopAtaque(territorios, numTerritorios);
+                printf("\nPressione Enter para continuar...\n");
+                getchar();
+                break;
+            }
+
+            case 2: {
+                verificarMissao(territorios, numTerritorios, jogador);
+
+                printf("\nPressione Enter para continuar...\n");
+                getchar();
+                break;
+            }
+
+            case 0:
+                printf("\nSaindo do jogo...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
+    } while (opcao != 0);
+}
+
+// --- Limpa o buffer do teclado ---
+void limpaBufferEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
